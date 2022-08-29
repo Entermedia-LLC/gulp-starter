03f238204963d972a1d5984f73b23da20c5c0e56
Ben Marshall <ben.s.marshall@icloud.com>
2022-08-29 13:32:50 -0500

    initial commit

---
+++ src/js/global.js
@@ -0,0 +1,14 @@
+/**
+ * Drupal behavior example
+ *
+ * @see https://www.drupal.org/docs/drupal-apis/javascript-api/javascript-api-overview
+ */
+/**
+(function ($, Drupal) {
+  Drupal.behaviors.myModuleBehavior = {
+    attach: function (context) {
+      $(".element", context).hide();
+    }
+  };
+})(jQuery, Drupal);
+ */
